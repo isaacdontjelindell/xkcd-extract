3e42f7c98d58417f1c75d8068c4f47be04c80e2d
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-03-08 11:23:32 -0600

    fix some formatting

--- crawler.py
+++ crawler.py
@@ -15,48 +15,48 @@ dest_folder = 'img'
 alt_texts = {}
 
 def getImages():
-	os.chdir( dest_folder)
+    os.chdir( dest_folder)
 
-	for i in range(1,10):
-		url =  'http://xkcd.com/' + str(i)
-		
-		with urllib.request.urlopen(url) as conn:
-			page = conn.read()
+    for i in range(1,10):
+        url =  'http://xkcd.com/' + str(i)
+        
+        with urllib.request.urlopen(url) as conn:
+            page = conn.read()
 
-		soup = BeautifulSoup(page)
-		image_tags = soup.find_all('img')
+        soup = BeautifulSoup(page)
+        image_tags = soup.find_all('img')
 
-		for img in image_tags:
-			# img[src] is the contents of the src field
-			# http://imgs.xkcd.com/comics/xxxx.png
-			if 'comics' in img['src']: 
-				log(INFO, 'Getting ' + img['src'])
-				
-				# this if/else kinda sucks - is there a better way to detect image type?
-				filename = ''
-				if '.jpg' in  img['src']:
-					filename = str(i) + '.jpg'
-				elif '.png' in img['src']:
-					filename = str(i) + '.png'
-				else:
-					log(ERROR, 'Did not recognize image format: ' + img['src'])
-					break
-				
-				urllib.request.urlretrieve(img['src'], filename)
-				alt_texts[filename] = img['title']
-				break
+        for img in image_tags:
+            # img[src] is the contents of the src field
+            # http://imgs.xkcd.com/comics/xxxx.png
+            if 'comics' in img['src']: 
+                log(INFO, 'Getting ' + img['src'])
+                
+                # this if/else kinda sucks - is there a better way to detect image type?
+                filename = ''
+                if '.jpg' in  img['src']:
+                    filename = str(i) + '.jpg'
+                elif '.png' in img['src']:
+                    filename = str(i) + '.png'
+                else:
+                    log(ERROR, 'Did not recognize image format: ' + img['src'])
+                    break
+                
+                urllib.request.urlretrieve(img['src'], filename)
+                alt_texts[filename] = img['title']
+                break
 
-	saveAltTexts()
+    saveAltTexts()
 
 def saveAltTexts():
-	with open('alt.yaml', 'w') as f:
-		yaml.dump(alt_texts, f)
+    with open('alt.yaml', 'w') as f:
+        yaml.dump(alt_texts, f)
 
 def log(level, msg):
-	if log_level == INFO:
-		print(level + ': ' + msg)
-	elif log_level == ERROR:
-		if level == ERROR:
-			print(level + ': ' + msg)
+    if log_level == INFO:
+        print(level + ': ' + msg)
+    elif log_level == ERROR:
+        if level == ERROR:
+            print(level + ': ' + msg)
 
 getImages()
